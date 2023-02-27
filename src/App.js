7e641af5dbc5ecb17a23760fa5c37adf948f7e40
Tuan Dao <75282610+TuanDao-0110@users.noreply.github.com>
2023-02-27 12:18:55 +0200

    tuan made some error function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+// Tuan made error 
 
 function App() {
+  const render =(banner,idea)=>{
+  console.log(banner)
+  }
   return (
     <div className="App">
       <h1>SonarQube test</h1>
       <p>Lera was here</p>
+    {render('hello world')}
     </div>
   );
 }
